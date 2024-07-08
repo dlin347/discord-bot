b1f0273dd489e1d41eb409fbe94dcdf96c14014b
Dawei Lin <personal.dawei.lin@gmail.com>
2024-07-08 22:22:25 +0200

    Update display member information command

--- commands/information/member.js
+++ commands/information/member.js
@@ -1,16 +1,18 @@
 const translation = require('../../locales/other/translation.js');
 
 module.exports = async function memberInformation(interaction) {
-    // To Do: Embed and response
+    const localeFile = await translation(interaction.locale);
+
     const member = interaction.options.getMember('member') || interaction.member;
-    console.log(member.id)
-    console.log(member.user.username)
-    console.log(member.displayName === member.user.username ? 'None Translated' : member.displayName)
-    console.log(member.user.createdAt.toLocaleString(interaction.locale)) // To Do: Display other format of date
-    console.log(member.joinedAt.toLocaleString(interaction.locale)) // To Do: Display other format of date
-    console.log(member.boostingSince ? member.premiumSince.toLocaleString(interaction.locale) : 'No Translated') // To Do: Display date of boosting since with translated response
-    console.log(member.user.bot ? 'Yes Translated' : 'No Translated')
-    console.log(member.permissions.toArray().join(', ')) // To Do: Limit
-    console.log(member.roles.cache.map(r => r.name).join(', ')) // To Do: Limit
-    console.log(member.user.avatarURL({ dynamic: true, size: 4096 }) ?? member.user.defaultAvatarURL)
+    const id = member.id;
+    const username = member.user.username;
+    const displayName = member.displayName === member.user.username ? localeFile.categories.information.commands.member.responses.embed.none : member.displayName;
+    const joinedTimestamp = Math.floor(member.joinedTimestamp / 1000);
+    const createdTimestamp = Math.floor(member.user.createdTimestamp / 1000);
+    const premiumSinceTimestamp = localeFile.categories.information.commands.member.responses.embed.since.replace('{{timestamp}}', Math.floor(member.premiumSinceTimestamp / 1000)) || localeFile.categories.information.commands.member.responses.embed.no; // This is wrong, need to work on it
+    const bot = member.user.bot ? localeFile.categories.information.commands.member.responses.embed.yes : localeFile.categories.information.commands.member.responses.embed.no;
+    const permissions = member.permissions.toArray().join(', '); //Limit
+    const roles = member.roles.cache.map(r => r.name).join(', '); // Limit
+    const avatar = member.user.avatarURL({ dynamic: true, size: 4096 }) || member.user.defaultAvatarURL;
+    const defaultError = localeFile.categories.information.commands.member.responses.defaultError.replace('{{member}}', `<@${member.id}>`);
 }
